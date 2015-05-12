7c6925cf05e83b8a2c2853a927da4b561fb6748e
jef210 <jmalek210@gmail.com>
2015-05-12 12:02:45 -0600

    text edit

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -7,6 +7,38 @@
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
    
-   
+// console logging testing the scroll on the window
+
+   $(window).scroll(function() {
+    console.log( "Firing!" );
+  });
+
+// var timer;  
+// var windowHeight = $(window).height();  
+// var triggerHeight = 0.5 * windowHeight;
+
+// $(window).scroll(function() {
+//     if(timer) {
+//         window.clearTimeout(timer);
+//     }
+
+//     timer = window.setTimeout(function() {
+
+//         // this variable changes between callbacks, so we can't cache it
+//         var y = $(window).scrollTop() + windowHeight;
+
+//         if(y > triggerHeight) {
+//             // ...
+//         }
+
+
+//     }, 100);
+});
+    
+});
+
+
+  
+});
 
 })(jQuery);
